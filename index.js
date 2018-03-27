b4adec3ca72a1b05b77312f8c6454778cb50a6d2
drhansen000 <drhansen000@hotmail.com>
2018-03-26 20:47:12 -0600

    user can now create account, login and logout!

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 var hbs = require('express-handlebars');
 var session = require('express-session');
 const firebase = require('firebase');
+const bodyParser = require('body-parser');
 var app = express();
 
 var db = require('./db');
@@ -18,6 +19,7 @@ app.engine('hbs', hbs({
     ]
 }));
 app.set('view engine', 'hbs');
+app.use(bodyParser.json());
 
 
 /*
@@ -47,20 +49,30 @@ var getCurrentDate = () => {
     return (currentDate = year + "-" + month + "-" + day);
 }
 
-app.get(['/', '/offer'], (req, res) => {
-    res.render('offer.hbs');
+var loggedIn = (res, template) => {
+    var user = firebase.auth().currentUser;
+    console.log(user);
+    if (user) {
+        res.render(template, { loggedIn: true });
+    } else {
+        res.render(template, { loggedIn: false });
+    }
+};
+
+app.get(['/', '/services'], (req, res) => {
+    loggedIn(res, 'offer.hbs');
 });
 
 app.get('/products', (req, res) => {
-    res.render('products.hbs');
+    loggedIn(res, 'products.hbs');
 });
 
 app.get('/create-account', (req, res) => {
-    res.render('createAccount.hbs');
+    loggedIn(res, 'createAccount.hbs');
 });
 
 app.get(['/create-appointment', '/update-appointment'], (req, res) => {
-    res.render('createAppointment.hbs');
+    loggedIn(res, 'createAppointment.hbs');
 });
 
 //app.get('/update-appointment', (req, res) => {
@@ -68,134 +80,117 @@ app.get(['/create-appointment', '/update-appointment'], (req, res) => {
 //});
 
 app.get('/login', (req, res) => {
-    res.render('login.hbs');
+    loggedIn(res, 'login.hbs');
 });
 app.get('/purchases', (req, res) => {
-    res.render('purchases.hbs');
+    loggedIn(res, 'purchases.hbs');
 });
 
 app.get('/appointments', (req, res) => {
-    res.render('appointments.hbs');
+    loggedIn(res, 'appointments.hbs');
 });
 
 app.get('/cart', (req, res) => {
-    res.render('cart.hbs');
+    loggedIn(res, 'cart.hbs');
 });
 
 /*
 * These endpoints are to view the information
 */
 app.get('/past-appointments-query', (req, res) => {
-    firebase.auth().onAuthStateChanged(function (user) {
-        if (user) {
-            var pastAppRef = db.db.ref(`/appointment/${user.uid}`).orderByChild('date');
-            pastAppRef.once('value', (snapshot) => {
-                var filteredPastApp = new Array();
-                snapshot.forEach((childSnapshot) => {
-                    if (childSnapshot.val().date < getCurrentDate()) {
-                        filteredPastApp.push(childSnapshot);
-                    }
-                });
+    var user = firebase.auth().currentUser;
 
-                filteredPastApp = reverseSnapshot(filteredPastApp);
-                res.send(filteredPastApp);
-            }).catch((e) => {
-                console.log(e);
+    if (user) {
+        var pastAppRef = db.db.ref(`/appointment/${user.uid}`).orderByChild('date');
+        pastAppRef.once('value', (snapshot) => {
+            var filteredPastApp = new Array();
+            snapshot.forEach((childSnapshot) => {
+                if (childSnapshot.val().date < getCurrentDate()) {
+                    filteredPastApp.push(childSnapshot);
+                }
             });
-        } else {
-            console.log('Not signed in!');
-        }
-    });
+
+            filteredPastApp = reverseSnapshot(filteredPastApp);
+            res.status(200).send(filteredPastApp);
+        }).catch((e) => {
+            console.log(e);
+        });
+    }
 });
 
 app.get('/product-history-query', (req, res) => {
-    firebase.auth().onAuthStateChanged(function (user) {
-        if (user) {
-            var productHistoryRef = db.db.ref(`/purchase_history/${user.uid}`);
-            productHistoryRef.once('value', (snapshot) => {
-                var filteredPurchaseHistory = reverseSnapshot(snapshot);
-                res.send(filteredPurchaseHistory);
-            }).catch((e) => {
-                console.log(e);
-            });
-        } else {
-            console.log('Not signed in!');
-        }
-    });
+    var user = firebase.auth().currentUser;
+
+    if (user) {
+        var productHistoryRef = db.db.ref(`/purchase_history/${user.uid}`);
+        productHistoryRef.once('value', (snapshot) => {
+            var filteredPurchaseHistory = reverseSnapshot(snapshot);
+            res.status(200).send(filteredPurchaseHistory);
+        }).catch((e) => {
+            console.log(e);
+        });
+    }
 });
 
 app.get('/future-appointments-query', (req, res) => {
-    firebase.auth().onAuthStateChanged(function (user) {
-        if (user) {
-            var futureAppRef = db.db.ref(`/appointment/${user.uid}`).orderByChild('date');
-            futureAppRef.once('value', (snapshot) => {
-                var filteredFutureApp = new Array();
+    var user = firebase.auth().currentUser;
 
-                snapshot.forEach((childSnapshot) => {
-                    if (childSnapshot.val().date >= getCurrentDate()) {
-                        filteredFutureApp.push(childSnapshot);
-                    }
-                });
-                res.json(filteredFutureApp);
-            }).catch((e) => {
-                console.log(e);
+    if (user) {
+        var futureAppRef = db.db.ref(`/appointment/${user.uid}`).orderByChild('date');
+        futureAppRef.once('value', (snapshot) => {
+            var filteredFutureApp = new Array();
+
+            snapshot.forEach((childSnapshot) => {
+                if (childSnapshot.val().date >= getCurrentDate()) {
+                    filteredFutureApp.push(childSnapshot);
+                }
             });
-        } else {
-            console.log('Not signed in!');
-        }
-    });
+            res.status(200).json(filteredFutureApp);
+        }).catch((e) => {
+            console.log(e);
+        });
+    }
 });
 
 app.get('/services-query', (req, res) => {
     console.log(req.query.serviceId);
     var serviceRef = db.db.ref(`/service/${req.query.serviceId}`);
     serviceRef.once('value', (snapshot) => {
-        res.send(snapshot);
+        res.status(200).send(snapshot);
     }).catch((e) => {
         console.log(e);
         });
 });
 
 app.get('/manager-clients-query', (req, res) => {
-    firebase.auth().onAuthStateChanged(function (user) {
-        if (user) {
             var clientRef = db.db.ref('/person');
             clientRef.once('value', (snapshot) => {
-                res.send(snapshot)
+                res.status(200).send(snapshot)
             }).catch((e) => {
                 console.log(e);
             });
-        } else {
-            console.log('Not signed in!');
-        }
-    });
 });
 
 app.get('/manager-appointments-query', (req, res) => {
-    firebase.auth().onAuthStateChanged(function (user) {
-        if (user) {
             var appRef = db.db.ref('/appointment');
             appRef.once('value', (snapshot) => {
-                res.send(snapshot)
+                res.status(200).send(snapshot)
             }).catch((e) => {
                 console.log(e);
             });
-        } else {
-            console.log('Not signed in!');
-        }
-    });
 });
 
 /*
 * These endpoints are to insert information
 */
-app.get('/login-query', (req, res) => {
-            firebase.auth().signInWithEmailAndPassword(req.query.email, req.query.password)
-                .then((result) => {
-                    res.send({ status: 'ok' });
-                }).catch((error) => {
-                    res.send(error);
-                });
+app.post('/login-query', (req, res) => {
+    firebase.auth().signInWithEmailAndPassword(req.body.email, req.body.password)
+        .then(() => {
+            res.status(200).send({status: 'ok' });
+        })
+        .catch((error) => {
+            res.send({ status: 'invalid' });
+        });
 });
 
 app.get('/logout-query', (req, res) => {
@@ -208,70 +203,64 @@ app.get('/logout-query', (req, res) => {
 });
 
 app.get('/confirm-purchase-query', (req, res) => {
-    firebase.auth().onAuthStateChanged(function (user) {
-        if (user) {
-            var purchaseRef = db.db.ref(`/purchase_history/${user.uid}`).push({
-                product: `${req.query.name}`,
-                price: `${req.query.price}`,
-                picture: `${req.query.picture}`
-            });
-            res.send({ status: 'ok' });
-        } else {
-            console.log('Not signed in!');
-        }
-    });
+    var user = firebase.auth().currentUser;
+
+    if (user) {
+        var purchaseRef = db.db.ref(`/purchase_history/${user.uid}`).push({
+            product: `${req.query.name}`,
+            price: `${req.query.price}`,
+            picture: `${req.query.picture}`
+        });
+        res.status(200).send({ status: 'ok' });
+    } else {
+        res.status(200).send({ status: 'invalid' });
+    }
 });
 
-app.get('/create-user-query', (req, res) => {
-    var email = "trial04@example.com";
-    var password = "password";
-    var name = "John Doe";
+app.post('/create-user-query', (req, res) => {
+    var email    = req.body.email;
+    var password = req.body.password;
     firebase.auth().createUserWithEmailAndPassword(email, password)
         .then((userRecord) => {
-            console.log("Successfully created new user:", userRecord.displayName);
-
-            res.send({ status : 'ok' });
+            res.status(200).send({ status: 'ok' });
         })
         .catch((error) => {
-            console.log(error.message);
-            res.send(error.message);
+            res.send({ error: `${error.message}` });
         });
 });
 
-app.get('/create-person-query', (req, res) => {
-    firebase.auth().onAuthStateChanged(function (user) {
-        if (user) {
-            var personRef = db.db.ref(`/person/${user.uid}`).set({
-                name: `Steve`,
-                phone: `1234`,
-                email: `${user.email}`
-            });
-            res.send({ status: 'ok' });
-        } else {
-            // No user is signed in.
-        }
-    });
+app.post('/create-person-query', (req, res) => {
+    var user = firebase.auth().currentUser;
+    console.log(`We're creating a person`);
+    if (user) {
+        var personRef = db.db.ref(`/person/${user.uid}`).set({
+            name: `${req.body.name}`,
+            phone: `${req.body.phone}`,
+            email: `${req.body.email}`
+        });
+        res.status(200).send({ status: 'ok' });
+    } else {
+        res.status(200).send({ status: 'invalid' });
+    }
 });
 
 app.get('/create-appointment-query', (req, res) => {
-    firebase.auth().onAuthStateChanged(function (user) {
-        if (user) {
-            console.log(req.query);
-            var appRef = db.db.ref(`/appointment/${user.uid}`).push({
-                date: `${req.query.date}`,
-                info: `${req.query.info}`,
-                service: {
-                    name: `${req.query.service}`,
-                    cost: `${req.query.cost}`,
-                    duration: `${req.query.duration}`
+    var user = firebase.auth().currentUser;
+
+    if (user) {
+        console.log(req.query);
+        var appRef = db.db.ref(`/appointment/${user.uid}`).push({
+            date: `${req.query.date}`,
+            info: `${req.query.info}`,
+            service: {
+                name: `${req.query.service}`,
+                cost: `${req.query.cost}`,
+                duration: `${req.query.duration}`
             },
-                time: `${req.query.time}`
-            });
-            res.render('appointments.hbs');
-        } else {
-            console.log('Not signed in!');
-        }
-    });
+            time: `${req.query.time}`
+        });
+        res.render('appointments.hbs');
+    }
 });
 
 /*
@@ -279,60 +268,54 @@ app.get('/create-appointment-query', (req, res) => {
 * object, but since they're small, the user won't notice any performance decrease.
 */
 app.get('/update-appointment-query', (req, res) => {
-    firebase.auth().onAuthStateChanged(function (user) {
-        if (user) {
-            var appRef = db.db.ref(`/appointment/${user.uid}/${req.query.tokenId}`);
-            appRef.set({
-                date: `${req.query.date}`,
-                info: `${req.query.info}`,
-                service: `${req.query.service}`,
-                time: `${req.query.time}`
-            });
-            res.send({ status: 'ok' });
-        } else {
-            console.log('Not signed in!');
-        }
-    });
+    var user = firebase.auth().currentUser;
+
+    if (user) {
+        var appRef = db.db.ref(`/appointment/${user.uid}/${req.query.tokenId}`);
+        appRef.set({
+            date: `${req.query.date}`,
+            info: `${req.query.info}`,
+            service: `${req.query.service}`,
+            time: `${req.query.time}`
+        });
+        res.status(200).send({ status: 'ok' });
+    }
 });
 
 app.get('/manager-update-client-query', (req, res) => {
-    firebase.auth().onAuthStateChanged(function (user) {
-        if (user) {
-            var personRef = db.db.ref(`/person/${user.uid}`);
-            personRef.set({
-                name: `${req.query.name}`,
-                email: `${req.query.email}`,
-                phone: `${req.query.phone}`
-            });
-            res.send({ status: 'ok' });
-        } else {
-            console.log('Not signed in!');
-        }
-    });
+    var user = firebase.auth().currentUser;
+
+    if (user) {
+        var personRef = db.db.ref(`/person/${user.uid}`);
+        personRef.set({
+            name: `${req.query.name}`,
+            email: `${req.query.email}`,
+            phone: `${req.query.phone}`
+        });
+        res.status(200).send({ status: 'ok' });
+    }
 });
 
 /*
 * This endpoint is to remove information
 */
 app.get('/remove-appointment-query', (req, res) => {
-    firebase.auth().onAuthStateChanged(function (user) {
-        if (user) {
             //var removeRef = db.db.ref(`/appointment/${user.uid}`);
             //var removeQuery = removeRef.orderByChild('date').equalTo(req.query.serviceDate);
             //removeQuery.once('child_added', function (snapshot) {
             //    snapshot.ref.remove();
             //});
-            let ref = firebase.database().ref(`/appointment/${user.uid}`);
-            ref.orderByChild('date').equalTo(req.query.serviceDate).once('value', snapshot => {
-                let updates = {};
-                snapshot.forEach(child => updates[child.key] = null);
-                ref.update(updates);
-            });
-            res.render('appointments.hbs');
-        } else {
-            console.log('Not signed in!');
-        }
-    });
+    var user = firebase.auth().currentUser;
+
+    if (user) {
+        let ref = firebase.database().ref(`/appointment/${user.uid}`);
+        ref.orderByChild('date').equalTo(req.query.serviceDate).once('value', snapshot => {
+            let updates = {};
+            snapshot.forEach(child => updates[child.key] = null);
+            ref.update(updates);
+        });
+        res.render('appointments.hbs');
+    }
 });
 
 //make is so that I don't have to configure for every html page. Anything within the Public folder is visible
